8be0c9b9dc8b624d11186ed280951c28d8e7669f
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-07-12 23:23:06 -0300

    useState

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,25 +1,33 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton';
 import './styles.css';
 
 function SalesCard() {
+
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
+
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
+
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={minDate}
+                        onChange={(date: Date) => setMinDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
                 </div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={maxDate}
+                        onChange={(date: Date) => setMaxDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
@@ -39,6 +47,32 @@ function SalesCard() {
                         </tr>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td className="tc992">#341</td>
+                            <td className="tc576">28/06/2022</td>
+                            <td>Anakin</td>
+                            <td className="tc992">15</td>
+                            <td className="tc992">11</td>
+                            <td>R$ 55300.00</td>
+                            <td>
+                                <div className="dsmeta-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="tc992">#341</td>
+                            <td className="tc576">28/06/2022</td>
+                            <td>Anakin</td>
+                            <td className="tc992">15</td>
+                            <td className="tc992">11</td>
+                            <td>R$ 55300.00</td>
+                            <td>
+                                <div className="dsmeta-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
                         <tr>
                             <td className="tc992">#341</td>
                             <td className="tc576">28/06/2022</td>

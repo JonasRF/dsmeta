e8ec7f6cc4a0f9fe24a5a1f4b2fc70f5d0fe78ec
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-07-14 17:47:08 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repostories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository saleRepository;
+	
+
+	public List<Sale> findSales() {
+		List<Sale> list = saleRepository.findAll();
+		return list;
+	}
+}

4d563541b84f70d234c1652d9c9d1c7ca50fcd54
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-07-14 16:19:40 -0300

    Security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import Header from "./components/Header";
-import NotificationButton from "./components/NotificationButton";
 import SalesCard from "./components/SalesCard";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
         <section id="sales">
           <div className="dsmeta-container">
             <SalesCard />
-
           </div>
         </section>
       </main>
